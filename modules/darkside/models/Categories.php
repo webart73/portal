2f9b51b31e4dc48e72e975bf102d2f8e3fd0529b
webArt73 <webart73@gmail.com>
2017-11-24 12:04:17 +0400

    DarkSide  +Categories

---
+++ modules/darkside/models/Categories.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace app\modules\darkside\models;
+
+/**
+ * This is the model class for table "dvg73_categories".
+ *
+ * @property string $id
+ * @property string $parent_id
+ * @property string $category_title
+ */
+class Categories extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'dvg73_categories';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['parent_id', 'category_title'], 'required'],
+            [['parent_id'], 'integer'],
+            [['category_title'], 'string', 'max' => 64],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'parent_id' => 'Parent ID',
+            'category_title' => 'Category Title',
+        ];
+    }
+}
---
+++ modules/darkside/models/CategoriesSearch.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace app\modules\darkside\models;
+
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+
+/**
+ * CategoriesSearch represents the model behind the search form about `app\modules\darkside\models\Categories`.
+ */
+class CategoriesSearch extends Categories
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'parent_id'], 'integer'],
+            [['category_title'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Categories::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'parent_id' => $this->parent_id,
+        ]);
+
+        $query->andFilterWhere(['like', 'category_title', $this->category_title]);
+
+        return $dataProvider;
+    }
+}
